78de7b4f1ea12cdc256f3fcf4efe76b837a3b935
Louis <p19970408@gmail.com>
2025-07-03 16:22:01 +0800

    update brandclassic swiper

--- src/views/brand/classic/BrandClassicView.vue
+++ src/views/brand/classic/BrandClassicView.vue
@@ -21,14 +21,11 @@
           :slidesPerView="'auto'"
           :freeMode="true"
           :grabCursor="true"
-          :navigation="{
-            nextEl: '.brand-classic-next',
-            prevEl: '.brand-classic-prev',
-          }"
           :initial-slide="3"
           :modules="[Navigation, FreeMode]"
           class="brand-classic-swiper"
           @swiper="onSwiper"
+          @set-translate="setTranslate"
           ref="slide"
         >
           <div class="brand-classic-swiper-nav-line"></div>
@@ -60,10 +57,18 @@
               </div>
             </div>
           </swiper-slide>
-          <div class="swiper-button-prev brand-classic-prev" @click.stop>
+          <div
+            class="swiper-button-prev brand-classic-prev"
+            @click.stop="brandClick('prev')"
+            :style="buttonPrevComputed ? 'opacity:0.5' : 'opacity:1'"
+          >
             <img src="@/assets/img/other/prev.svg" />
           </div>
-          <div class="swiper-button-next brand-classic-next" @click.stop>
+          <div
+            class="swiper-button-next brand-classic-next"
+            @click.stop="brandClick('next')"
+            :style="buttonNextComputed ? 'opacity:0.5' : 'opacity:1'"
+          >
             <img src="@/assets/img/other/next.svg" />
           </div>
         </Swiper>
@@ -76,11 +81,10 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 import gsap from 'gsap'
 import { Swiper, SwiperSlide } from 'swiper/vue'
 import { Swiper as SwiperType } from 'swiper'
-// import { useSwiper } from 'swiper/vue'
 import { Navigation, FreeMode } from 'swiper/modules'
 import Nav from '@/components/nav/Nav.vue'
 import FadeIn from '@/components/transition/FadeIn.vue'
@@ -93,9 +97,9 @@ const fancyData = ref<null | { year: string; name: string; img: string }>(null)
 
 const slide = ref()
 
-// const nextClick = ref()
+const moveValue = ref(0)
 
-// const prevClick = ref()
+const swiperLimit = ref(0)
 
 const toProduct = (year?: string, id?: string) => {
   if (!year || !id) return (fancyData.value = null)
@@ -117,30 +121,35 @@ const onSwiper = (swiper: SwiperType) => {
   slide.value.$el.querySelector('.swiper-wrapper').style['transition-timing-function'] =
     ' cubic-bezier(.63,0,.3,1)'
 
+  //取得最大距離
+  const swiperLength = swiper.snapGrid.length
+  swiperLimit.value = swiper.snapGrid[swiperLength - 1]
+
   gsapInit(swiper)
   setTimeout(() => {
     swiper.slideTo(0, 1000)
   }, 500)
 }
 
-// const testChange = (swiper: SwiperType) => {
-//   // console.log(swiper)
-//   // swiper.setTranslate(-2000)
-// }
+const setTranslate = ({ translate }: { translate: number }) => {
+  moveValue.value = translate
+}
 
-// const testTransition = (swiper: SwiperType) => {
-//   // swiper.setTranslate(-2000)
-// }
-
-// const testClick = (swiper: SwiperType, event: MouseEvent | TouchEvent | PointerEvent) => {
-//   // console.log(swiper)
-//   // console.log(event)
-// }
-
-// const brandClick = (e: any) => {
-//   // const testSwiper = useSwiper()
-//   // slide.value.setTranslate(-500)
-// }
+//自製輪播按鈕
+const brandClick = (val: string) => {
+  if (val === 'next') {
+    moveValue.value -= 750
+  } else {
+    moveValue.value += 750
+  }
+  if (moveValue.value > 0) {
+    moveValue.value = 0
+  } else if (moveValue.value < -swiperLimit.value) {
+    moveValue.value = -swiperLimit.value
+  }
+  slide.value.$el.swiper.setTransition(500)
+  slide.value.$el.swiper.setTranslate(moveValue.value)
+}
 
 const gsapInit = (swiper: SwiperType) => {
   const tl = gsap.timeline({ delay: 0.6 })
@@ -167,6 +176,16 @@ const gsapInit = (swiper: SwiperType) => {
     '<',
   )
 }
+
+const buttonPrevComputed = computed(() => {
+  const result = moveValue.value < 0 ? false : true
+  return result
+})
+
+const buttonNextComputed = computed(() => {
+  const result = moveValue.value > -swiperLimit.value ? false : true
+  return result
+})
 </script>
 
 <style scoped></style>
