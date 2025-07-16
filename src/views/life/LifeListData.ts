d5bca7c99b688560a8a2e9e47c6a7a31cb8479cc
Louis <p19970408@gmail.com>
2025-07-16 17:59:49 +0800

    fix home-view-link and lifelistdata img name

--- src/views/life/LifeListData.ts
+++ src/views/life/LifeListData.ts
@@ -320,14 +320,14 @@ export const LifeSwiperData = [
       {
         key: 'img-1',
         img: new URL(
-          '@/assets/img/life/prosperous-district/興豐路商圈/商圈-興豐路商圈日景 (2).webp',
+          '@/assets/img/life/prosperous-district/sing-fong-lu-district/sing-fong-lu-district-day(2).webp',
           import.meta.url,
         ).href,
       },
       {
         key: 'img-2',
         img: new URL(
-          '@/assets/img/life/prosperous-district/興豐路商圈/商圈-興豐路商圈夜景 (3).webp',
+          '@/assets/img/life/prosperous-district/sing-fong-lu-district/sing-fong-lu-district-night(3).webp',
           import.meta.url,
         ).href,
       },
@@ -340,10 +340,8 @@ export const LifeSwiperData = [
     images: [
       {
         key: 'img-1',
-        img: new URL(
-          '@/assets/img/life/prosperous-district/藏壽司/商圈-藏壽司 (1).webp',
-          import.meta.url,
-        ).href,
+        img: new URL('@/assets/img/life/prosperous-district/kura/kura(1).webp', import.meta.url)
+          .href,
       },
     ],
   },
@@ -367,7 +365,7 @@ export const LifeSwiperData = [
       {
         key: 'img-1',
         img: new URL(
-          '@/assets/img/life/行政醫療/八德戶政事務所/行政中心-八德戶政事務所 (1).webp',
+          '@/assets/img/life/administrative-medical/bade-HRO/bade-HRO(1).webp',
           import.meta.url,
         ).href,
       },
@@ -381,7 +379,7 @@ export const LifeSwiperData = [
       {
         key: 'img-1',
         img: new URL(
-          '@/assets/img/life/行政醫療/八德北區青少年活動中心/行政中心-八德北區青少年活動中心 (1).webp',
+          '@/assets/img/life/administrative-medical/bade-YAC/bade-YAC(1).webp',
           import.meta.url,
         ).href,
       },
@@ -395,7 +393,7 @@ export const LifeSwiperData = [
       {
         key: 'img-1',
         img: new URL(
-          '@/assets/img/life/行政醫療/八德社會住宅/行政中心-八德社會住宅 (2).webp',
+          '@/assets/img/life/administrative-medical/bade-SH/bade-SH(2).webp',
           import.meta.url,
         ).href,
       },
@@ -409,7 +407,7 @@ export const LifeSwiperData = [
       {
         key: 'img-1',
         img: new URL(
-          '@/assets/img/life/行政醫療/八德社會住宅圖書館/行政中心-八德社會住宅圖書館 (1).webp',
+          '@/assets/img/life/administrative-medical/bade-SHL/bade-SHL(1).webp',
           import.meta.url,
         ).href,
       },
@@ -423,7 +421,7 @@ export const LifeSwiperData = [
       {
         key: 'img-1',
         img: new URL(
-          '@/assets/img/life/行政醫療/八德農會+區公所/行政中心-八德農會+區公所 (1).webp',
+          '@/assets/img/life/administrative-medical/bade-FA+DO/bade-FA+DO(1).webp',
           import.meta.url,
         ).href,
       },
@@ -437,7 +435,7 @@ export const LifeSwiperData = [
       {
         key: 'img-1',
         img: new URL(
-          '@/assets/img/life/行政醫療/八德興豐社福館/行政中心-八德興豐社福館 (1).webp',
+          '@/assets/img/life/administrative-medical/bade-SWO/bade-SWO(1).webp',
           import.meta.url,
         ).href,
       },
@@ -450,8 +448,10 @@ export const LifeSwiperData = [
     images: [
       {
         key: 'img-1',
-        img: new URL('@/assets/img/life/交通脈絡/國道二號/交通-國道二號 (4).webp', import.meta.url)
-          .href,
+        img: new URL(
+          '@/assets/img/life/trans/national-freeway-02/national-freeway-02(4).webp',
+          import.meta.url,
+        ).href,
       },
     ],
   },
@@ -462,10 +462,7 @@ export const LifeSwiperData = [
     images: [
       {
         key: 'img-1',
-        img: new URL(
-          '@/assets/img/life/交通脈絡/捷運綠線/交通-捷運綠線G01站 (1).webp',
-          import.meta.url,
-        ).href,
+        img: new URL('@/assets/img/life/trans/metro/metro(1).webp', import.meta.url).href,
       },
     ],
   },
@@ -475,12 +472,18 @@ export const LifeSwiperData = [
     images: [
       {
         key: 'para-1',
-        newsIcon: new URL('@/assets/img/life/行政醫療/醫院/聯合報@2x.webp', import.meta.url).href,
+        newsIcon: new URL(
+          '@/assets/img/life/administrative-medical/hospitial/union@2x.webp',
+          import.meta.url,
+        ).href,
         title: '桃園首座市立醫院 確定選址八德',
         time: '2025-02-13 01:15 聯合報／ 記者朱冠諭／桃園報導',
         para: '桃園市是唯一沒有市立醫院的直轄市，市長張善政昨公布桃園市立醫院選址，將落腳八德區霄裡公園，未來採BOT案方式推動，投資金額將超過91.4億元，目標2033年營運。不少民代分享喜訊，但大溪復興仍欠缺醫療資源，盼未來增設分院。\n\n桃園市近年人口增長迅速，醫療資源相對匱乏，桃園原有2個次醫療區病床床位皆已額滿。桃市府去年爭取重新畫分次醫療區域有成，今年過完年後，盼來衛福部公告在桃園市新增「平德」次醫療區，張善政昨在市政會議說，經評估19個潛在地點後，拍板將在八德霄裡公園設置市立醫院。',
         source: '資料來源: 聯合新聞網\n https://udn.com/news/story/7324/8544518',
-        paraImg: new URL('@/assets/img/life/行政醫療/醫院/市立醫院@2x.webp', import.meta.url).href,
+        paraImg: new URL(
+          '@/assets/img/life/administrative-medical/hospitial/municipal-hospital@2x.webp',
+          import.meta.url,
+        ).href,
       },
     ],
   },
@@ -490,13 +493,19 @@ export const LifeSwiperData = [
     images: [
       {
         key: 'para-1',
-        newsIcon: new URL('@/assets/img/life/行政醫療/醫院/中時@2x.webp', import.meta.url).href,
+        newsIcon: new URL(
+          '@/assets/img/life/administrative-medical/hospitial/china@2x.webp',
+          import.meta.url,
+        ).href,
         title: '中央大學八德校區設立醫院 預計2027年營運',
         time: '2021-04-08 14:25 中時新聞網／ 記者呂筱蟬／桃園報導',
         para: '中央大學籌設八德校區，其中，中大醫院設立計畫書已通過桃園市醫事審議委員會審核，近期將送衛生福利部審查，預計在今年完成BOT招商與簽約，2027年底開始營運。\n\n校方指出，中央大學八德校區以發展預防及精準醫學為核心，打造「智慧健康創新園區」佔地2.6公頃，包含有中大醫院、智慧健康科技園區、新創基地與國際人才培育中心。教育部於2020年8月同意中央大學籌設「八德校區」，校區土地亦於2021年1月獲行政院同意撥用，中大醫院設立也經桃園市醫審會通過，近期將送到衛福部審查，預計今年招商、簽約並開始實質開發，採促參BOT模式，引進民間資金進行興建與營運。',
         source:
           '資料來源: 中時新聞網\nhttps://www.chinatimes.com/realtimenews/20210408003155-260421?chdtv',
-        paraImg: new URL('@/assets/img/life/行政醫療/醫院/中大醫院@2x.webp', import.meta.url).href,
+        paraImg: new URL(
+          '@/assets/img/life/administrative-medical/hospitial/zhong-hospital@2x.webp',
+          import.meta.url,
+        ).href,
       },
     ],
   },
@@ -506,13 +515,19 @@ export const LifeSwiperData = [
     images: [
       {
         key: 'para-1',
-        newsIcon: new URL('@/assets/img/life/行政醫療/醫院/中時@2x.webp', import.meta.url).href,
+        newsIcon: new URL(
+          '@/assets/img/life/administrative-medical/hospitial/china@2x.webp',
+          import.meta.url,
+        ).href,
         title: '八德長榮醫院今天動土 預計2026年7月完工啟用',
         time: '2022-10-29 17:48 中時新聞網／ 記者陳夢茹／桃園報導',
         para: '桃園八德長榮醫院今天舉行動土典禮，市長鄭文燦出席典禮時表示，八德長榮醫院將設置在綠線捷運G02站附近，共規畫13項科別、238床病床，規模為中型醫院，預計2026年7月可完工啟用，由於多數大型醫院集中在北桃園地區，盼八德長榮醫院未來建置完畢後能就近照顧八德、大溪及復興的市民朋友。\n盤點桃園市內的大型醫院，部立桃園醫院位於桃園區，林口長庚醫院更緊鄰新北市，對南桃園地區的市民來說，就醫路途相當遙遠，許多市議員盼能爭取在靠近南區的位置設置醫院。\n\n八德長榮醫院目前預計畫設238床病床，包括一般急性床150床及特殊床88床，另規畫13項科別，分別為家庭醫學科、兒科、內科、外科、急診醫學科、復健科、骨科、麻醉科、神經科、泌尿科、眼科、婦產科和放射診斷科。',
         source:
           '資料來源: 中時新聞網\nhttps://www.chinatimes.com/realtimenews/20221029002802-260405?chdtv',
-        paraImg: new URL('@/assets/img/life/行政醫療/醫院/長榮醫院@2x.webp', import.meta.url).href,
+        paraImg: new URL(
+          '@/assets/img/life/administrative-medical/hospitial/EVA-hospital@2x.webp',
+          import.meta.url,
+        ).href,
       },
     ],
   },
